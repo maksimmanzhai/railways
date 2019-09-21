525146b84bc876df9d0e8ceb828bbce56e04408e
Maksim Manzhai <bender23max@gmail.com>
2019-09-21 20:30:39 +0300

    Ending route class

---
+++ route.rb
@@ -0,0 +1,24 @@
+class Route
+  attr_reader :list_of_stations
+
+  def initialize(start, finish)
+    @start = start
+    @finish = finish
+    @list_of_stations = []
+    @list_of_stations << @start
+    @list_of_stations << @finish
+  end
+
+  def add_station(station)
+    @list_of_stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @list_of_stations.reject! {|a| a == station}
+  end
+
+  def list_of_stations
+    puts @list_of_stations
+  end
+
+end
